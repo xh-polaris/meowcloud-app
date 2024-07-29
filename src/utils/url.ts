336a26353463ed19fd4ba003cfc8486094840336
LiuZhihan <167763586+LLLzzh@users.noreply.github.com>
2024-07-30 01:18:52 +0800

    chore: 配置pages和utils

---
+++ src/utils/url.ts
@@ -0,0 +1,11 @@
+//将字符串替换成常量引用
+export const enum Pages{
+    MeowAlbum = '/pages/album/meow-album',
+    AlbumDetail = '/pages/album/album-detail',
+    AlbumSettings = '/pages/album/album-settings',
+    PhotoDetail = '/pages/album/photo-detail',
+    TimeAlbum = '/pages/album/time-album',
+    FirstPage = '/pages/first-page/first-page',
+    MeowPlanet = '/pages/meow-planet/meow-planet',
+    Profile = '/pages/profile/profile'
+}
