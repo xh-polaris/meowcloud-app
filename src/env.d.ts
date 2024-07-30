f42908abd1096023cfb5f8ac2d912f00621fab5d
LiuZhihan <167763586+LLLzzh@users.noreply.github.com>
2024-07-31 01:44:48 +0800

    chore: 重构项目从vue2到vue3

---
+++ src/env.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="vite/client" />
+
+declare module "*.vue" {
+    import { DefineComponent } from "vue";
+    const component: DefineComponent<
+        Record<string, unknown>,
+        Record<string, unknown>,
+        Record<string, unknown>
+    >;
+    export default component;
+}
+
+// 环境变量声明
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+    readonly VITE_BASIC_URL: string;
+    readonly VITE_NAME: string;
+    readonly VITE_ENABLE_DEBUG: boolean;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
