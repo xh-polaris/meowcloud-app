fffde6ff2755c0142ebb87c757b8e7337f3328f1
LiuZhihan <167763586+LLLzzh@users.noreply.github.com>
2024-07-31 01:33:29 +0800

    chore: 配置pages和utils

---
+++ src/utils/const.ts
@@ -0,0 +1,28 @@
+export const enum StorageKeys {
+    CommunityId = "communityId",
+    UserId = "userId",
+    AccessToken = "accessToken",
+    HistoryCampuses = "historyCampuses",
+    IdSelected = "idSelected",
+    NameSelected = "nameSelected",
+    AvatarSelected = "avatarSelected",
+    DraftMoment = "draftMoment",
+    DraftPost = "draftPost",
+    BackendEnv = "backendEnv",
+    BackendLane = "backendLane",
+    EnabledDebug = "enableDebug",
+    HistorySearchText = "historySearchText"
+}
+
+export const enum BackendEnv {
+    Product = "pro",
+    Test = "test"
+}
+
+export const BackendEnvMap = {
+    release: BackendEnv.Product,
+    trial: BackendEnv.Test,
+    develop: BackendEnv.Test
+};
+
+export const AppId = 1;
