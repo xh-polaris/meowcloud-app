bc484346c554dee650e32471e01decc5317976d9
LiuZhihan <167763586+LLLzzh@users.noreply.github.com>
2024-08-01 01:16:15 +0800

    chore: 重构项目到vue3

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,11 +1,11 @@
-//将字符串替换成常量引用
-export const enum Pages{
-    MeowAlbum = '/pages/album/meow-album',
-    AlbumDetail = '/pages/album/album-detail',
-    AlbumSettings = '/pages/album/album-settings',
-    PhotoDetail = '/pages/album/photo-detail',
-    TimeAlbum = '/pages/album/time-album',
-    FirstPage = '/pages/first-page/first-page',
-    MeowPlanet = '/pages/meow-planet/meow-planet',
-    Profile = '/pages/profile/profile'
+export const enum Pages {
+    MyAlbum = "/pages/album/my-album",
+    TeamSpace = "/pages/album/team-space",
+    TimeAlbum = "/pages/album/time-album",
+    AlbumSettings = "/pages/album/album-settings",
+    AlbumDetails = "/pages/album/album-details",
+    AlbumComments = "/pages/album/album-comments",
+    PhotoDetails = "/pages/album/photo-details",
+    MeowPlanet = "/pages/meow-planet/meow-planet",
+    Profile = "/pages/profile/profile",
 }
