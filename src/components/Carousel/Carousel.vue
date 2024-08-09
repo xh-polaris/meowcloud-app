582210fc1243f363449517beb77abfa4c170e4a5
LiuZhihan <167763586+LLLzzh@users.noreply.github.com>
2024-08-10 03:20:13 +0800

    feat: 完成相册-我的相册静态页面

--- src/components/Carousel/Carousel.vue
+++ src/components/Carousel/Carousel.vue
@@ -5,7 +5,11 @@
 <template>
   <view class="carousel">
     <view class="carousel-content">
-      <view class="interesting-functions"></view>
+      <view class="interesting-functions">
+        <text class="interesting-functions-text">
+          趣味功能
+        </text>
+      </view>
       <view class="right"></view>
     </view>
   </view>
@@ -23,12 +27,24 @@
     .carousel-content {
       display: flex;
       .interesting-functions {
+        display: flex;
         width: 408rpx;
         height: 108rpx;
         margin-left: 20rpx;
         margin-top: 18rpx;
         background-color: #d9d9d9;
         border-radius: 4rpx;
+        color: #000;
+        .interesting-functions-text {
+          margin-left: 136rpx;
+          margin-top: 32rpx;
+          width: 104rpx;
+          height: 44rpx;
+          font-size: 26rpx;
+          line-height: 44rpx;
+          font-weight: 400;
+          color: #000;
+        }
       }
       .right {
         width: 240rpx;
---
+++ src/components/CatItem/CatItem.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <view class="cat-item">
+    <image
+      class="cat-icon"
+      src="/static/images/cat.jpg"
+      mode="aspectFill"
+    />
+    <view class="cat-count">
+      <text class="cat-count-text">
+        {{ 12 }}
+      </text>
+    </view>
+    <view class="cat-name">
+      {{"猫猫"}}
+    </view>
+  </view>
+</template>
+
+<style scoped lang="scss">
+  .cat-item {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    width: 144rpx;
+    height: 208rpx;
+    gap: 20rpx;
+    .cat-icon {
+      width: 144rpx;
+      height: 144rpx;
+      border-radius: 72rpx;
+    }
+    .cat-count {
+      display: flex;
+      position: relative;
+      top: -46rpx;
+      left: 90rpx;
+      width: 54rpx;
+      height: 28rpx;
+      border-radius: 16rpx;
+      border: 2rpx solid #fff;
+      background-color: #0094ff;
+      align-items: center;
+      justify-content: center;
+      .cat-count-text {
+        text-align: center;
+        width: 22rpx;
+        height: 44rpx;
+        font-weight: 500;
+        font-size: 18rpx;
+        line-height: 44rpx;
+        color: #fff;
+      }
+    }
+    .cat-name {
+      margin-top: -46rpx;
+      width: 144rpx;
+      height: 44rpx;
+      font-weight: 400;
+      font-size: 28rpx;
+      line-height: 44rpx;
+      color: #000;
+      text-align: center;
+    }
+  }
+</style>
