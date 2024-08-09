8ceb59edab37f56a317040ed0a311df5851ab1ee
LiuZhihan <167763586+LLLzzh@users.noreply.github.com>
2024-08-09 21:27:21 +0800

    feat: 完成轮播图区域雏形

--- src/pages/album/my-album.vue
+++ src/pages/album/my-album.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   import BottomBar from "@/components/BottomBar/BottomBar.vue";
   import TopBar from "@/components/TopBar/TopBar.vue";
+  import Carousel from "@/components/Carousel/Carousel.vue";
 </script>
 
 <template>
   <top-bar id="my-album"/>
+  <carousel/>
   <bottom-bar id="album"/>
 </template>
 
